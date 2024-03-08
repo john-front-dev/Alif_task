fcdc30608d43d41cef688f485799a59c3bd50c5f
john_front_dev <jamshid200404@gmail.com>
2024-03-08 10:24:07 +0500

    create

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 import DataLoadingComponent from "./components/DataLoadingComponent ";
 import DataTableComponent from "./components/DataTableComponent";
 import UserCreationForm from "./components/UserCreationForm";
@@ -6,15 +13,34 @@ import UserCreationForm from "./components/UserCreationForm";
 function App() {
   const [users, setUsers] = useState([]);
   const [hasMore, setHasMore] = useState(true);
+  const [open, setOpen] = useState(false);
 
   const handleCreateUser = (newUser) => {
     setUsers((prevUsers) => [...prevUsers, newUser]);
+    handleCloseModal();
   };
-  
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
 
   return (
     <div>
-      <UserCreationForm onCreateUser={handleCreateUser} />
+      <Button variant="contained" onClick={handleOpenModal}>
+        Создать пользователя
+      </Button>
+      <Dialog open={open} onClose={handleCloseModal}>
+        <DialogContent>
+          <UserCreationForm
+            onCreateUser={handleCreateUser}
+            handleCloseModal={handleCloseModal}
+          />
+        </DialogContent>
+      </Dialog>
       <DataLoadingComponent setUsers={setUsers} setHasMore={setHasMore} />
       <DataTableComponent users={users} />
     </div>
--- src/components/UserCreationForm/index.jsx
+++ src/components/UserCreationForm/index.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
 import { createUser } from "../../api/api";
+import {
+  Button,
+  TextField,
+  Grid,
+  Typography,
+  Snackbar,
+  DialogActions,
+} from "@mui/material";
+import { Alert } from "@mui/material";
 
-const UserCreationForm = ({ onCreateUser }) => {
-  const [newUserName, setNewUserName] = useState(""); // Состояние для имени нового пользователя
-  const [newUserEmail, setNewUserEmail] = useState(""); // Состояние для email нового пользователя
-  const [newUserStreet, setNewUserStreet] = useState(""); // Состояние для улицы нового пользователя
+const UserCreationForm = ({ onCreateUser, handleCloseModal }) => {
+  const [newUserName, setNewUserName] = useState("");
+  const [newUserEmail, setNewUserEmail] = useState("");
+  const [newUserStreet, setNewUserStreet] = useState("");
+  const [newUserPhone, setNewUserPhone] = useState("");
+  const [newUserUsername, setNewUserUsername] = useState("");
+  const [newUserWebsite, setNewUserWebsite] = useState("");
+  const [openToast, setOpenToast] = useState(false);
+  const [phoneError, setPhoneError] = useState(false); // Состояние ошибки для поля телефонного номера
 
-  // Обработчик отправки формы
   const handleSubmit = async (e) => {
     e.preventDefault();
     const newUser = {
@@ -15,46 +28,128 @@ const UserCreationForm = ({ onCreateUser }) => {
       address: {
         street: newUserStreet,
       },
+      phone: newUserPhone,
+      username: newUserUsername,
+      website: newUserWebsite,
     };
 
     try {
-      await createUser(newUser); // Создание нового пользователя
-      setNewUserName(""); // Очистка состояния имени
-      setNewUserEmail(""); // Очистка состояния email
-      setNewUserStreet(""); // Очистка состояния улицы
-      onCreateUser(newUser); // Вызов колбэка для обновления списка пользователей
+      await createUser(newUser);
+      setNewUserName("");
+      setNewUserEmail("");
+      setNewUserStreet("");
+      setNewUserPhone("");
+      setNewUserUsername("");
+      setNewUserWebsite("");
+      onCreateUser(newUser);
+      setOpenToast(true);
+      handleCloseModal(); // Закрываем модальное окно после успешного создания пользователя
     } catch (error) {
-      console.error("Ошибка при создании пользователя:", error); // Обработка ошибок
+      console.error("Ошибка при создании пользователя:", error);
     }
+  };
+
+  const handleCloseToast = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenToast(false);
+  };
+
+  const handlePhoneChange = (e) => {
+    const value = e.target.value;
+    setNewUserPhone(value);
+    setPhoneError(!(/^\d+$/.test(value) && value.length === 12));
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div>
-        <label>Имя:</label>
-        <input
-          type="text"
-          value={newUserName}
-          onChange={(e) => setNewUserName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Email:</label>
-        <input
-          type="text"
-          value={newUserEmail}
-          onChange={(e) => setNewUserEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label>Улица:</label>
-        <input
-          type="text"
-          value={newUserStreet}
-          onChange={(e) => setNewUserStreet(e.target.value)}
-        />
-      </div>
-      <button type="submit">Создать</button>
+      <Grid container spacing={2} justifyContent="end">
+        <Grid item xs={12}>
+          <Typography variant="h6">Создание пользователя</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            label="Имя"
+            value={newUserName}
+            onChange={(e) => setNewUserName(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            label="Email"
+            type="email"
+            value={newUserEmail}
+            onChange={(e) => setNewUserEmail(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            label="Улица"
+            value={newUserStreet}
+            onChange={(e) => setNewUserStreet(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            label="Номер телефона"
+            value={newUserPhone}
+            onChange={handlePhoneChange}
+            error={phoneError}
+            helperText={phoneError ? "Неверный формат номера телефона" : ""}
+            inputProps={{ type: "number" }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Ник"
+            value={newUserUsername}
+            onChange={(e) => setNewUserUsername(e.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            label="Сайт"
+            value={newUserWebsite}
+            onChange={(e) => setNewUserWebsite(e.target.value)}
+          />
+        </Grid>
+        <DialogActions>
+          <Button
+            variant="contained"
+            color="inherit"
+            onClick={handleCloseModal}
+          >
+            Закрыть
+          </Button>
+          <Button type="submit" variant="contained" color="primary">
+            Создать
+          </Button>
+        </DialogActions>
+      </Grid>
+      <Snackbar
+        open={openToast}
+        autoHideDuration={6000}
+        onClose={handleCloseToast}
+      >
+        <Alert
+          onClose={handleCloseToast}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          Пользователь успешно создан!
+        </Alert>
+      </Snackbar>
     </form>
   );
 };
